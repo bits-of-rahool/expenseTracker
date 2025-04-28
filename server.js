2b27a906c3eb8be905f9341e9fd1754b24004116
Rahul Dhakad <dhakad706@gmail.com>
2025-04-28 11:52:53 +0530

    remvoed morgan

--- server.js
+++ server.js
@@ -1,14 +1,12 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const cors = require('cors');
-const morgan = require('morgan');
 require('dotenv').config();
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
-app.use(morgan('dev'));
 
 // Db
 mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/expense-tracker')
